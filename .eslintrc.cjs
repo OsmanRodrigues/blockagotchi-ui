85212dd28017cc6bf8be10e5f65f20e284590023
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-12 18:40:30 -0300

    chore: lint adjusts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,23 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:eslint-plugin-react/recommended',
-    'eslint-config-prettier'
-  ],
-  ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:eslint-plugin-react/recommended',
+        'eslint-config-prettier'
     ],
-  },
+    ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'eslint-plugin-react'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true }
+      ],
+      'react/react-in-jsx-scope': 0,
+      'no-unused-vars': 1,
+      '@typescript-eslint/no-unused-vars': 1
+    }
 };
