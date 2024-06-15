2c98da0cbef34e66dc75f58f0c3e307c9d4785f0
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-15 14:27:39 -0300

    chore: rollups context impl

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,6 +24,7 @@ module.exports = {
         ],
         'react/react-in-jsx-scope': 0,
         'no-unused-vars': 1,
-        'react/prop-types': 0
+        'react/prop-types': 0,
+        'react-refresh/only-export-components': 0
     }
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { WalletProvider } from './services/wallet/wallet.provider';
+import { RollupsProvider } from './services/rollups/rollups.context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <WalletProvider>
-            <App />
+            <RollupsProvider>
+                <App />
+            </RollupsProvider>
         </WalletProvider>
     </React.StrictMode>
 );
