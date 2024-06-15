84e4a1607bd1750364d1bf9d9df391062be40ccb
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-15 00:30:43 -0300

    chore: rollups integration try

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,21 +3,27 @@ module.exports = {
     env: { browser: true, es2020: true },
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
+        'plugin:react/jsx-runtime',
         'plugin:react-hooks/recommended',
         'plugin:eslint-plugin-react/recommended',
         'eslint-config-prettier'
     ],
-    ignorePatterns: ['dist', 'node_modules', '.eslintrc.cjs'],
-    parser: '@typescript-eslint/parser',
+    ignorePatterns: [
+        'dist',
+        'node_modules',
+        '.eslintrc.cjs',
+        'src/**/generated/**/*'
+    ],
     plugins: ['react-refresh', 'eslint-plugin-react'],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
     rules: {
         'react-refresh/only-export-components': [
             'warn',
             { allowConstantExport: true }
-      ],
-      'react/react-in-jsx-scope': 0,
-      'no-unused-vars': 1,
-      '@typescript-eslint/no-unused-vars': 1
+        ],
+        'react/react-in-jsx-scope': 0,
+        'no-unused-vars': 1,
+        'react/prop-types': 0
     }
 };
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import 'nes.css/css/nes.min.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.tsx';
-import { WalletProvider } from './services/wallet/wallet.provider.tsx';
+import App from './app';
+import { WalletProvider } from './services/wallet/wallet.provider';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <WalletProvider>
             <App />
