63e9b45ff1a85263015489243c0463699c6b861b
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-15 17:25:48 -0300

    chore: msg provider impl

---
+++ src/atomic/message.jsx
@@ -0,0 +1,105 @@
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useMemo,
+    useRef,
+    useState
+} from 'react';
+
+const MessageContext = createContext();
+
+export const MessageProvider = ({ children }) => {
+    const [messages, setMessages] = useState(new Map());
+    const timers = useRef([]);
+    const msgs = Array.from(messages, ([, value]) => value);
+
+    const show = useCallback((text = '', className = 'primary') => {
+        const id = Date.now();
+        const timerId = window.setInterval(() => {
+            setMessages(prev => {
+                const newMessages = new Map(prev);
+                const timerId = newMessages.get(id).timerId;
+                timers.current = timers.current.length
+                    ? [...timers.current, timerId]
+                    : [timerId];
+                newMessages.delete(id);
+                return newMessages;
+            });
+        }, 3000);
+        const message = {
+            id,
+            text,
+            timerId,
+            className: `is-${className}`
+        };
+
+        setMessages(prev => {
+            const newMessages = new Map(prev);
+            newMessages.set(id, message);
+            return newMessages;
+        });
+    }, []);
+    const value = useMemo(
+        () => ({
+            success: text => show(text, 'success'),
+            warning: text => show(text, 'warning'),
+            error: text => show(text, 'error'),
+            show: text => show(text)
+        }),
+        []
+    );
+
+    useEffect(() => {
+        return () => {
+            if (messages.size > 0 && timers.current.length) {
+                timers.current.forEach(id => window.clearInterval(id));
+                timers.current = [];
+            }
+        };
+    }, [messages]);
+
+    return (
+        <MessageContext.Provider value={value}>
+            {msgs.map(message => (
+                <section
+                    key={message.id}
+                    className="message-right"
+                    style={{
+                        position: 'fixed',
+                        top: 8,
+                        right: 8,
+                        zIndex: 10
+                    }}
+                >
+                    <div
+                        className="nes-balloon from-right"
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: 4
+                        }}
+                    >
+                        <span className="nes-badge" style={{ width: 16 }}>
+                            <span className={message.className}>.</span>
+                        </span>
+                        <p>{message.text}</p>
+                    </div>
+                </section>
+            ))}
+            {children}
+        </MessageContext.Provider>
+    );
+};
+
+export const useMessage = () => {
+    const context = useContext(MessageContext);
+
+    if (typeof context === 'undefined')
+        throw new Error(
+            'The useMessage hook must be consumed inside of a MessageProvider child node.'
+        );
+
+    return context;
+};
