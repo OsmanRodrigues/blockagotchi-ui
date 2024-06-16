d48896d322c04d0d4cf495251e1d0e38675ee5a3
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-15 23:49:47 -0300

    chore: blockagotchi load impl

---
+++ src/views/home/create-blockagotchi.jsx
@@ -0,0 +1,9 @@
+import { SectionWrapper } from '../../atomic/wrapper/section.wrapper';
+
+export const CreateBlockagotchiSection = () => {
+    return (
+        <SectionWrapper>
+            <p>Create section</p>
+        </SectionWrapper>
+    );
+};
--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,16 +1,58 @@
+import { useState, useEffect } from 'react';
+import { useRollups } from '../../services/rollups/rollups.context';
+import { getBlockagotchi } from '../../services/inspects';
+import { useMessage } from '../../atomic/message';
 import { ViewWrapper } from '../../atomic/wrapper/view.wrapper.atm';
+import { CreateBlockagotchiSection } from './create-blockagotchi';
 
 function HomeView() {
+    const message = useMessage();
+    const rollups = useRollups();
+    const [blockagotchi, setBlockagotchi] = useState({
+        status: 'idle'
+    });
+    const noBlockagotchiYet =
+        blockagotchi.status === 'success' && blockagotchi.data?.status === 404;
+
+    useEffect(() => {
+        if (rollups.signerAddress) {
+            setBlockagotchi({ status: 'pending' });
+            getBlockagotchi(rollups.signerAddress)
+                .then(res =>
+                    setBlockagotchi({
+                        status: 'success',
+                        data: res.data ?? undefined
+                    })
+                )
+                .catch(err => {
+                    if (err.status === 404)
+                        setBlockagotchi({
+                            status: 'success',
+                            data: { status: err.status }
+                        });
+                    else {
+                        message.error(
+                            'Oops! An error occurred while loading your blockagotchi.'
+                        );
+                        setBlockagotchi(prev => ({
+                            ...prev,
+                            status: 'error',
+                            error: err.error
+                        }));
+                    }
+                });
+        } else {
+            if (blockagotchi.data) setBlockagotchi({ status: 'idle' });
+        }
+    }, [rollups.signerAddress]);
+
     return (
         <ViewWrapper>
-            <div
-                style={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    padding: 12
-                }}
-            ></div>
-            <button className="nes-btn is-primary">create</button>
+            {noBlockagotchiYet ? (
+                <CreateBlockagotchiSection />
+            ) : (
+                <p>My blockagotchi: {blockagotchi.data?.name}</p>
+            )}
         </ViewWrapper>
     );
 }
