0be42734b4c42db5380b0f5b9afbb9b04a35caeb
OsmanRodrigues <osmancesar.mr@gmail.com>
2024-06-16 10:36:29 -0300

    chore: no connected fallback added

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -12,6 +12,7 @@ function HomeView() {
     const rollups = useRollups();
     const [accountBalance, getAccountBalance] = useAccountBalance();
     const [blockagotchi, loadBlockagotchi] = useBlockagotchiInfo();
+    const isFullConnected = rollups.signerAddress && rollups.provider;
     const noBlockagotchiYet =
         blockagotchi.status === 'success' && blockagotchi.data?.status === 404;
     const balanceAmount = accountBalance.data?.amount
@@ -20,25 +21,31 @@ function HomeView() {
 
     return (
         <ViewWrapper>
-            {balanceAmount === 0 ? (
-                <p>
-                    Sorry! Insufficient balance. You need to deposit some tokens
-                    to crate your blockagotchi.
-                </p>
-            ) : noBlockagotchiYet ? (
-                <CreateBlockagotchiSection
-                    onCreateSuccess={() => {
-                        loadBlockagotchi(rollups.signerAddress);
-                        getAccountBalance(rollups.signerAddress);
-                    }}
-                />
-            ) : blockagotchi.status === 'error' ? (
-                <p>Oops! Some error occurred while load your blockagotchi.</p>
+            {isFullConnected ? (
+                balanceAmount === 0 ? (
+                    <p>
+                        Sorry! Insufficient balance. You need to deposit some
+                        tokens to crate your blockagotchi.
+                    </p>
+                ) : noBlockagotchiYet ? (
+                    <CreateBlockagotchiSection
+                        onCreateSuccess={() => {
+                            loadBlockagotchi(rollups.signerAddress);
+                            getAccountBalance(rollups.signerAddress);
+                        }}
+                    />
+                ) : blockagotchi.status === 'error' ? (
+                    <p>
+                        Oops! Some error occurred while load your blockagotchi.
+                    </p>
+                ) : (
+                    <CareBlockagotchiSection
+                        isLoading={blockagotchi.status === 'pending'}
+                        {...blockagotchi.data}
+                    />
+                )
             ) : (
-                <CareBlockagotchiSection
-                    isLoading={blockagotchi.status === 'pending'}
-                    {...blockagotchi.data}
-                />
+                <p style={{ textAlign: 'center' }}>Not connected</p>
             )}
         </ViewWrapper>
     );
